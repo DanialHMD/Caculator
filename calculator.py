9538ff1fe230679ce4a78aa9e61a95acca9235d1
Danial Hamidi <108475573+DanialHMD@users.noreply.github.com>
2022-07-11 08:39:03 +0430

    Calculator with pyqt5

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QDesktopWidget
 import sys
 
-
+#code by Danial Hamidi
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
